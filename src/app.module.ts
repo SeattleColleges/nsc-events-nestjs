3b5be3592ca061bf530dc62e6f214055bba47766
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-08-14 10:38:53 -0700

    testing: changing import reference to UserModule to long form

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
+import { UserModule } from '../src/user/user.module';
 
 @Module({
   imports: [
