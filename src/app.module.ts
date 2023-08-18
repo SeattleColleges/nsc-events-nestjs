e813fccfe8c52b133339fc1307aa0806977ac583
taylorpapke <taylorpapke@gmail.com>
2023-08-18 15:59:38 -0700

    deleted app controller, services and imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { ActivityModule } from './activity/activity.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGODB_URI), ActivityModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(), 
+    MongooseModule.forRoot(process.env.MONGODB_URI), 
+    ActivityModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
