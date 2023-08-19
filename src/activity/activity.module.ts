2b0137d46f8236a153e1011eaa4b7fa502c89e33
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-08-18 23:53:43 -0700

    taylor: added MongooseModule.forFeature

--- src/activity/activity.module.ts
+++ src/activity/activity.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ActivityController } from './controllers/activity/activity.controller';
 import { ActivityService } from './services/activity/activity.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ActivitySchema } from './types/activity.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Activity', schema: ActivitySchema }]),
+  ],
   controllers: [ActivityController],
-  providers: [ActivityService]
+  providers: [ActivityService],
 })
 export class ActivityModule {}
