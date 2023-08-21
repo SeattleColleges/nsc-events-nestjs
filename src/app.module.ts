3b7cd0af5d66dd984eba62196dbf094b42bb4625
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-08-20 20:23:02 -0700

    deleted default app module, controller and the test.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGODB_URI)],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
