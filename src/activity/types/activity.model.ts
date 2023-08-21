4760e12cf5f57ae99700def99ab106e584b4748a
taylorpapke <taylorpapke@gmail.com>
2023-08-21 15:50:08 -0700

    fixing error

--- src/activity/types/activity.model.ts
+++ src/activity/types/activity.model.ts
@@ -22,7 +22,7 @@ export interface ActivityDocument extends Document {
   eventPrerequisites: string;
   eventCancellationPolicy: string;
   eventContact: string;
-  eventSocialMedia: [], //{
+  eventSocialMedia: []; //{
   //   facebook: string;
   //   twitter: string;
   //   instagram: string;
