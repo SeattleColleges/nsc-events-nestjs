01c0967fcc388e2d4b666545fbca8236425a2bac
taylorpapke <taylorpapke@gmail.com>
2023-08-20 21:14:57 -0700

    integrated event model into activity controller and service

--- src/activity/services/activity/activity.service.ts
+++ src/activity/services/activity/activity.service.ts
@@ -30,31 +30,209 @@ export class ActivityService {
       throw new HttpException('Activity not found!', 404);
     }
     return {
+      eventCreatorId: activity.eventCreatorId,
       eventTitle: activity.eventTitle,
+      eventDescription: activity.eventDescription,
       eventCategory: activity.eventCategory,
+      eventDate: activity.eventDate,
+      eventStartTime: activity.eventStartTime,
+      eventEndTime: activity.eventEndTime,
+      eventLocation: activity.eventLocation,
+      eventCoverPhoto: activity.eventCoverPhoto,
+      eventHost: activity.eventHost,
+      eventWebsite: activity.eventWebsite,
+      eventRegistration: activity.eventRegistration,
+      eventCapacity: activity.eventCapacity,
+      eventCost: activity.eventCost,
       eventTags: activity.eventTags,
+      eventSchedule: activity.eventSchedule,
+      eventSpeakers: activity.eventSpeakers,
+      eventPrerequisites: activity.eventPrerequisites,
+      eventCancellationPolicy: activity.eventCancellationPolicy,
+      eventContact: activity.eventContact,
+      eventSocialMedia: activity.eventSocialMedia,
+      eventPrivacy: activity.eventPrivacy,
+      eventAccessibility: activity.eventAccessibility
     } as ActivityDocument;
   }
 
-  async addEvent(eventTitle: string, eventCategory: string, eventTags: string[]): Promise<any> {
-    const newEvent = new this.activityModel({ eventTitle, eventCategory, eventTags});
+  async addEvent(
+    eventCreatorId: string,
+    eventTitle: string,
+    eventDescription: string,
+    eventCategory: string,
+    eventDate: Date,
+    eventStartTime: string,
+    eventEndTime: string,
+    eventLocation: string,
+    eventCoverPhoto: string,
+    eventHost: string,
+    eventWebsite: string,
+    eventRegistration: string,
+    eventCapacity: string,
+    eventCost: string,
+    eventTags: string[],
+    eventSchedule: string,
+    eventSpeakers: string[],
+    eventPrerequisites: string,
+    eventCancellationPolicy: string,
+    eventContact: string,
+    eventSocialMedia: [],
+    eventPrivacy: string,
+    eventAccessibility: string,
+  ): Promise<any> {
+    const newEvent = new this.activityModel({ 
+      eventCreatorId,
+      eventTitle,
+      eventDescription,
+      eventCategory,
+      eventDate,
+      eventStartTime,
+      eventEndTime,
+      eventLocation,
+      eventCoverPhoto,
+      eventHost,
+      eventWebsite,
+      eventRegistration,
+      eventCapacity,
+      eventCost,
+      eventTags,
+      eventSchedule,
+      eventSpeakers,
+      eventPrerequisites,
+      eventCancellationPolicy,
+      eventContact,
+      eventSocialMedia,
+      eventPrivacy,
+      eventAccessibility,
+    });
     const result = await newEvent.save();
     return result._id;
   }
 
-  async updateActivity(id: string, eventTitle: string, eventCategory: string, eventTags: string[]) {
+  async updateActivity(
+    id: string, 
+    eventCreatorId: string,
+    eventTitle: string,
+    eventDescription: string,
+    eventCategory: string,
+    eventDate: Date,
+    eventStartTime: string,
+    eventEndTime: string,
+    eventLocation: string,
+    eventCoverPhoto: string,
+    eventHost: string,
+    eventWebsite: string,
+    eventRegistration: string,
+    eventCapacity: string,
+    eventCost: string,
+    eventTags: string[],
+    eventSchedule: string,
+    eventSpeakers: string[],
+    eventPrerequisites: string,
+    eventCancellationPolicy: string,
+    eventContact: string,
+    eventSocialMedia: {
+      "facebook": string,
+      "twitter": string,
+      "instagram": string,
+      "hashtag": string
+    },
+    eventPrivacy: string,
+    eventAccessibility: string,
+    ) {
     const updatedActivity = await this.activityModel.findById(id).exec();
+    if (eventCreatorId) {
+      console.log('eventCreatorId ', eventCreatorId);
+      updatedActivity.eventCreatorId = eventCreatorId;
+    }
     if (eventTitle) {
       console.log('eventTitle ', eventTitle);
       updatedActivity.eventTitle = eventTitle;
+    }
+    if (eventDescription) {
+      console.log('eventDescription ', eventDescription);
+      updatedActivity.eventDescription = eventDescription;
     }
     if (eventCategory) {
       console.log('eventCategory ', eventCategory);
       updatedActivity.eventCategory = eventCategory;
     }
+    if (eventDate) {
+      console.log('eventDate ', eventDate);
+      updatedActivity.eventDate = eventDate;
+    }
+    if (eventStartTime) {
+      console.log('eventStartTime ', eventStartTime);
+      updatedActivity.eventStartTime = eventStartTime;
+    }
+    if (eventEndTime) {
+      console.log('eventEndTime ', eventEndTime);
+      updatedActivity.eventEndTime = eventEndTime;
+    }
+    if (eventLocation) {
+      console.log('eventLocation ', eventLocation);
+      updatedActivity.eventLocation = eventLocation;
+    }
+    if (eventCoverPhoto) {
+      console.log('eventCoverPhoto ', eventCoverPhoto);
+      updatedActivity.eventCoverPhoto = eventCoverPhoto;
+    }
+    if (eventHost) {
+      console.log('eventHost ', eventHost);
+      updatedActivity.eventHost = eventHost;
+    }
+    if (eventWebsite) {
+      console.log('eventWebsite ', eventWebsite);
+      updatedActivity.eventWebsite = eventWebsite;
+    }
+    if (eventRegistration) {
+      console.log('eventRegistration ', eventRegistration);
+      updatedActivity.eventRegistration = eventRegistration;
+    }
+    if (eventCapacity) {
+      console.log('eventCapacity ', eventCapacity);
+      updatedActivity.eventCapacity = eventCapacity;
+    }
+    if (eventCost) {
+      console.log('eventCost ', eventCost);
+      updatedActivity.eventCost = eventCost;
+    }
     if (eventTags) {
       console.log('eventTags ', eventTags);
       updatedActivity.eventTags = eventTags;
+    }
+    if (eventSchedule) {
+      console.log('eventSchedule ', eventSchedule);
+      updatedActivity.eventSchedule = eventSchedule;
+    }
+    if (eventSpeakers) {
+      console.log('eventSpeakers ', eventSpeakers);
+      updatedActivity.eventSpeakers = eventSpeakers;
+    }
+    if (eventPrerequisites) {
+      console.log('eventPrerequisites ', eventPrerequisites);
+      updatedActivity.eventPrerequisites = eventPrerequisites;
+    }
+    if (eventCancellationPolicy) {
+      console.log('eventCancellationPolicy ', eventCancellationPolicy);
+      updatedActivity.eventCancellationPolicy = eventCancellationPolicy;
+    }
+    if (eventContact) {
+      console.log('eventContact ', eventContact);
+      updatedActivity.eventContact = eventContact;
+    }
+    if (eventSocialMedia) {
+      console.log('eventSocialMedia ', eventSocialMedia);
+      updatedActivity.eventSocialMedia = eventSocialMedia;
+    }
+    if (eventPrivacy) {
+      console.log('eventPrivacy ', eventPrivacy);
+      updatedActivity.eventPrivacy = eventPrivacy;
+    }
+    if (eventAccessibility) {
+      console.log('eventAccessibility ', eventAccessibility);
+      updatedActivity.eventAccessibility = eventAccessibility;
     }
     const updated = await updatedActivity.save();
     console.log(updated);
--- src/activity/types/activity.model.ts
+++ src/activity/types/activity.model.ts
@@ -34,57 +34,21 @@ export interface ActivityDocument extends Document {
 
 // uncomment ActivitySchema below when ready to integrate entire model
 
-// export const ActivitySchema = new Schema<ActivityDocument>({
-//   eventCreatorId: { type: String, required: true, unique: false },
-//   eventTitle: { type: String, required: true, unique: true },
-//   eventDescription: { type: String, required: true },
-//   eventCategory: { type: String, required: false },
-//   eventDate: { type: Date, required: true },
-//   eventStartTime: { type: String, required: true },
-//   eventEndTime: { type: String, required: true },
-//   eventLocation: { type: String, required: true },
-//   eventCoverPhoto: { type: String, required: true },
-//   eventHost: { type: String, required: false },
-//   eventWebsite: { type: String, required: true },
-//   eventRegistration: { type: String, required: true },
-//   eventCapacity: { type: String, required: true },
-//   eventCost: { type: String, required: true },
-//   eventTags: {
-//     type: [],
-//     required: false,
-//     default: ['pizza'],
-//   },
-//   eventSchedule: { type: String, required: false },
-//   eventSpeakers: [],
-//   eventPrerequisites: { type: String, required: false },
-//   eventCancellationPolicy: {
-//     type: String,
-//     required: false,
-//     default: undefined,
-//   },
-//   eventContact: { type: String, required: false },
-//   eventSocialMedia: { type: [], required: false },
-//   eventPrivacy: { type: String, required: false },
-//   eventAccessibility: { type: String, required: true },
-// });
-
-// comment out ActivitySchema below and uncomment ActivitySchema above when ready to integrate entire model
-
 export const ActivitySchema = new Schema<ActivityDocument>({
-  eventCreatorId: { type: String, required: false, unique: false },
+  eventCreatorId: { type: String, required: true, unique: false },
   eventTitle: { type: String, required: true, unique: true },
-  eventDescription: { type: String, required: false },
+  eventDescription: { type: String, required: true },
   eventCategory: { type: String, required: false },
-  eventDate: { type: Date, required: false },
-  eventStartTime: { type: String, required: false },
-  eventEndTime: { type: String, required: false },
-  eventLocation: { type: String, required: false },
-  eventCoverPhoto: { type: String, required: false },
+  eventDate: { type: Date, required: true },
+  eventStartTime: { type: String, required: true },
+  eventEndTime: { type: String, required: true },
+  eventLocation: { type: String, required: true },
+  eventCoverPhoto: { type: String, required: true },
   eventHost: { type: String, required: false },
-  eventWebsite: { type: String, required: false },
-  eventRegistration: { type: String, required: false },
-  eventCapacity: { type: String, required: false },
-  eventCost: { type: String, required: false },
+  eventWebsite: { type: String, required: true },
+  eventRegistration: { type: String, required: true },
+  eventCapacity: { type: String, required: true },
+  eventCost: { type: String, required: true },
   eventTags: {
     type: [],
     required: false,
@@ -101,8 +65,44 @@ export const ActivitySchema = new Schema<ActivityDocument>({
   eventContact: { type: String, required: false },
   eventSocialMedia: { type: [], required: false },
   eventPrivacy: { type: String, required: false },
-  eventAccessibility: { type: String, required: false },
+  eventAccessibility: { type: String, required: true },
 });
+
+// comment out ActivitySchema below and uncomment ActivitySchema above when ready to integrate entire model
+
+// export const ActivitySchema = new Schema<ActivityDocument>({
+//   eventCreatorId: { type: String, required: false, unique: false },
+//   eventTitle: { type: String, required: true, unique: true },
+//   eventDescription: { type: String, required: false },
+//   eventCategory: { type: String, required: false },
+//   eventDate: { type: Date, required: false },
+//   eventStartTime: { type: String, required: false },
+//   eventEndTime: { type: String, required: false },
+//   eventLocation: { type: String, required: false },
+//   eventCoverPhoto: { type: String, required: false },
+//   eventHost: { type: String, required: false },
+//   eventWebsite: { type: String, required: false },
+//   eventRegistration: { type: String, required: false },
+//   eventCapacity: { type: String, required: false },
+//   eventCost: { type: String, required: false },
+//   eventTags: {
+//     type: [],
+//     required: false,
+//     default: ['pizza'],
+//   },
+//   eventSchedule: { type: String, required: false },
+//   eventSpeakers: [],
+//   eventPrerequisites: { type: String, required: false },
+//   eventCancellationPolicy: {
+//     type: String,
+//     required: false,
+//     default: undefined,
+//   },
+//   eventContact: { type: String, required: false },
+//   eventSocialMedia: { type: [], required: false },
+//   eventPrivacy: { type: String, required: false },
+//   eventAccessibility: { type: String, required: false },
+// });
 
 const ActivityModel = models.Activity || model('Activity', ActivitySchema);
 export default ActivityModel as Model<ActivityDocument /*{}, Methods*/>;
