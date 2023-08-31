5ea78990c76242ed9515c7307ab5cd94793f1665
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-08-31 11:08:15 -0700

    enabled cors: TODO restrict which origins can access backend

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors(/*{
+  // TODO: set env variable here
+    origin: 'http://localhost:3001', // the frontend (next.js) server
+  }*/);
   await app.listen(3000);
 }
 bootstrap();
