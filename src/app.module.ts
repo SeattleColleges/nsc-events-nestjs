b93bdb064aedfdb7e3ab38dc686c3bb43a61da9e
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-08-31 10:26:18 -0700

    added: ActivityModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { ActivityModule } from './activity/activity.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     UserModule,
+    ActivityModule,
   ],
   controllers: [],
   providers: [],
