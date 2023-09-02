843820f5c7f041867c7cb89b06392be4f3d406ef
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-09-01 21:21:18 -0700

    added comments on eventSocialMedia

--- src/activity/types/activity.model.ts
+++ src/activity/types/activity.model.ts
@@ -22,7 +22,7 @@ export interface ActivityDocument extends Document {
   eventPrerequisites: string;
   eventCancellationPolicy: string;
   eventContact: string;
-  eventSocialMedia: object; //{
+  eventSocialMedia: object; //{ these are key-value pairs so will use an object
   //   facebook: string;
   //   twitter: string;
   //   instagram: string;
