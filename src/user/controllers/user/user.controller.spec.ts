8f0da72217270acc1c2d555c71503d80aed9f67c
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-09-01 22:37:15 -0700

    inferred mockUserService type from usage

--- src/user/controllers/user/user.controller.spec.ts
+++ src/user/controllers/user/user.controller.spec.ts
@@ -5,7 +5,14 @@ import { Role } from '../../types/user.model';
 
 describe('UserController', () => {
   let controller: UserController;
-  let mockUserService;
+  let mockUserService: {
+    addUser: any;
+    getAllUsers: any;
+    getUserById: any;
+    getUserByEmail: any;
+    updateUser: any;
+    removeUser?: jest.Mock<any, any, any>;
+  };
 
   // TODO: move mockUsers to their own file in test/mock-data
   const mockUsers = [
