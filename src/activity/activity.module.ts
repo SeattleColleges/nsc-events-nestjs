f71ee93671884b9154f6cfbc17e61f2cfc6993cc
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-09-02 23:00:36 -0700

    refactor: changed schema file path

--- src/activity/activity.module.ts
+++ src/activity/activity.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ActivityController } from './controllers/activity/activity.controller';
 import { ActivityService } from './services/activity/activity.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ActivitySchema } from './types/activity.model';
+import { ActivitySchema } from './schemas/activity.schema';
 
 @Module({
   imports: [
