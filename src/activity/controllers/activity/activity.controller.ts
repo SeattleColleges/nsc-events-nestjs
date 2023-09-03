33fac1a41ccc4ebee38a5171916ae219c0126f59
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-09-02 22:58:15 -0700

    added: query activities, pagination. Renamed routes. Added DTOs

--- src/activity/controllers/activity/activity.controller.ts
+++ src/activity/controllers/activity/activity.controller.ts
@@ -1,29 +1,50 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { ActivityService } from '../../services/activity/activity.service';
-import { ActivityDto } from '../../types/activity.dto';
+import { Activity } from '../../schemas/activity.schema';
+import { Types } from 'mongoose';
+import { CreateActivityDto } from '../../dto/create-activity.dto';
+import { UpdateActivityDto } from '../../dto/update-activity.dto';
+import { Query as ExpressQuery } from 'express-serve-static-core';
 
-@Controller('activity')
+@Controller('events')
 export class ActivityController {
   constructor(private readonly activityService: ActivityService) {}
   @Get('')
-  async getAllActivities() {
-    return await this.activityService.getAllActivities();
+  async getAllActivities(@Query() query: ExpressQuery): Promise<Activity[]> {
+    return await this.activityService.getAllActivities(query);
   }
 
-  @Get(':id')
-  async getActivityById(@Param('id') id: string) {
-    console.log(id);
+  @Get('find/:id')
+  async findActivityById(@Param('id') id: string): Promise<Activity> {
     return this.activityService.getActivityById(id);
   }
 
-  @Post('add')
-  async addEvent(@Body() activity: ActivityDto) {
-    const newEvent = await this.activityService.addEvent(activity);
-    return { id: newEvent };
+  @Post('new')
+  async addEvent(
+    @Body() activity: CreateActivityDto,
+  ): Promise<Activity & { _id: Types.ObjectId }> {
+    return await this.activityService.addEvent(activity);
   }
 
-  @Patch('update/:id')
-  async updateActivity(@Param('id') id: string, @Body() activity: ActivityDto) {
-    await this.activityService.updateActivity(id, activity);
+  @Put('update/:id')
+  async updateActivityById(
+    @Param('id') id: string,
+    @Body() activity: UpdateActivityDto,
+  ): Promise<Activity & { _id: Types.ObjectId }> {
+    return await this.activityService.updateActivityById(id, activity);
+  }
+
+  @Delete('remove/:id')
+  async deleteActivityById(@Param('id') id: string): Promise<Activity> {
+    return this.activityService.deleteActivityById(id);
   }
 }
