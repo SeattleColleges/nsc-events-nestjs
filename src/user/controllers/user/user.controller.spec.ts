6f3a13ca5af44b32fc9c6e8e2e7d6b3bd213e134
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-09-05 10:04:31 -0700

    commented these files: WIP

--- src/user/controllers/user/user.controller.spec.ts
+++ src/user/controllers/user/user.controller.spec.ts
@@ -1,100 +1,100 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UserController } from './user.controller';
-import { UserService } from '../../services/user/user.service';
-import { Role } from '../../types/user.model';
-
-describe('UserController', () => {
-  let controller: UserController;
-  let mockUserService: {
-    addUser: any;
-    getAllUsers: any;
-    getUserById: any;
-    getUserByEmail: any;
-    updateUser: any;
-    removeUser?: jest.Mock<any, any, any>;
-  };
-
-  // TODO: move mockUsers to their own file in test/mock-data
-  const mockUsers = [
-    { id: '1', name: 'Alice', email: 'alice@example.com', role: 'user' },
-    { id: '2', name: 'Bob', email: 'bob@example.com', role: 'admin' },
-    // Add more mock users as needed
-  ];
-
-  beforeEach(async () => {
-    mockUserService = {
-      addUser: jest
-        .fn()
-        .mockImplementation((/*name, email, role*/) =>
-          Promise.resolve('some_id')),
-      getAllUsers: jest.fn().mockReturnValue(mockUsers), // Define getAllUsers here
-      getUserById: jest
-        .fn()
-        .mockImplementation((id) => mockUsers.find((user) => user.id === id)),
-      getUserByEmail: jest.fn().mockReturnValue('some_email'),
-      updateUser: jest
-        .fn()
-        .mockImplementation((/*id, name, email, role*/) => Promise.resolve()),
-      removeUser: jest.fn(),
-    };
-
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController],
-      providers: [
-        {
-          provide: UserService,
-          useValue: mockUserService,
-        },
-      ],
-    }).compile();
-
-    controller = module.get<UserController>(UserController);
-  });
-
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-
-  it('should add a user', async () => {
-    const name = 'John';
-    const email = 'john@example.com';
-    const role = 'user';
-    const result = await controller.addUser(name, email, role);
-    expect(result.id).toEqual('some_id');
-    expect(mockUserService.addUser).toHaveBeenCalledWith(name, email, role);
-  });
-
-  it('should get all users', async () => {
-    mockUserService.getAllUsers = jest.fn().mockReturnValue(mockUsers);
-    const users = await controller.getAllUsers();
-    expect(users).toEqual(mockUsers);
-  });
-
-  it('should get user by ID', async () => {
-    const userId = '1';
-    const user = await controller.getUserById(userId);
-    expect(user).toEqual(mockUsers[0]);
-    expect(mockUserService.getUserById).toHaveBeenCalledWith(userId);
-  });
-
-  it('should get user by email', async () => {
-    const email = 'alice@example.com';
-    const user = await controller.getUserByEmail(email);
-    expect(user).toEqual('some_email');
-    expect(mockUserService.getUserByEmail).toHaveBeenCalledWith(email);
-  });
-
-  it('should update a user', async () => {
-    const id = '1';
-    const name = 'Updated Name';
-    const email = 'updated@example.com';
-    const role = Role.Admin;
-    await controller.updateUser(id, name, email, role);
-    expect(mockUserService.updateUser).toHaveBeenCalledWith(
-      id,
-      name,
-      email,
-      role,
-    );
-  });
-});
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { UserController } from './user.controller';
+// import { UserService } from '../../services/user/user.service';
+// import { Role } from '../../schemas/user.model';
+//
+// describe('UserController', () => {
+//   let controller: UserController;
+//   let mockUserService: {
+//     addUser: any;
+//     getAllUsers: any;
+//     getUserById: any;
+//     getUserByEmail: any;
+//     updateUser: any;
+//     removeUser?: jest.Mock<any, any, any>;
+//   };
+//
+//   // TODO: move mockUsers to their own file in test/mock-data
+//   const mockUsers = [
+//     { id: '1', name: 'Alice', email: 'alice@example.com', role: 'user' },
+//     { id: '2', name: 'Bob', email: 'bob@example.com', role: 'admin' },
+//     // Add more mock users as needed
+//   ];
+//
+//   beforeEach(async () => {
+//     mockUserService = {
+//       addUser: jest
+//         .fn()
+//         .mockImplementation((/*name, email, role*/) =>
+//           Promise.resolve('some_id')),
+//       getAllUsers: jest.fn().mockReturnValue(mockUsers), // Define getAllUsers here
+//       getUserById: jest
+//         .fn()
+//         .mockImplementation((id) => mockUsers.find((user) => user.id === id)),
+//       getUserByEmail: jest.fn().mockReturnValue('some_email'),
+//       updateUser: jest
+//         .fn()
+//         .mockImplementation((/*id, name, email, role*/) => Promise.resolve()),
+//       removeUser: jest.fn(),
+//     };
+//
+//     const module: TestingModule = await Test.createTestingModule({
+//       controllers: [UserController],
+//       providers: [
+//         {
+//           provide: UserService,
+//           useValue: mockUserService,
+//         },
+//       ],
+//     }).compile();
+//
+//     controller = module.get<UserController>(UserController);
+//   });
+//
+//   it('should be defined', () => {
+//     expect(controller).toBeDefined();
+//   });
+//
+//   it('should add a user', async () => {
+//     const name = 'John';
+//     const email = 'john@example.com';
+//     const role = 'user';
+//     const result = await controller.addUser(name, email, role);
+//     expect(result.id).toEqual('some_id');
+//     expect(mockUserService.addUser).toHaveBeenCalledWith(name, email, role);
+//   });
+//
+//   it('should get all users', async () => {
+//     mockUserService.getAllUsers = jest.fn().mockReturnValue(mockUsers);
+//     const users = await controller.getAllUsers();
+//     expect(users).toEqual(mockUsers);
+//   });
+//
+//   it('should get user by ID', async () => {
+//     const userId = '1';
+//     const user = await controller.getUserById(userId);
+//     expect(user).toEqual(mockUsers[0]);
+//     expect(mockUserService.getUserById).toHaveBeenCalledWith(userId);
+//   });
+//
+//   it('should get user by email', async () => {
+//     const email = 'alice@example.com';
+//     const user = await controller.getUserByEmail(email);
+//     expect(user).toEqual('some_email');
+//     expect(mockUserService.getUserByEmail).toHaveBeenCalledWith(email);
+//   });
+//
+//   it('should update a user', async () => {
+//     const id = '1';
+//     const name = 'Updated Name';
+//     const email = 'updated@example.com';
+//     const role = Role.admin;
+//     await controller.updateUser(id, name, email, role);
+//     expect(mockUserService.updateUser).toHaveBeenCalledWith(
+//       id,
+//       name,
+//       email,
+//       role,
+//     );
+//   });
+// });
--- src/user/services/user/user.service.ts
+++ src/user/services/user/user.service.ts
@@ -1,79 +1,79 @@
-import { HttpException, Injectable } from '@nestjs/common';
-import { Model } from 'mongoose';
-import { Role, UserDocument } from '../../types/user.model';
-import { InjectModel } from '@nestjs/mongoose';
-
-@Injectable()
-export class UserService {
-  constructor(@InjectModel('User') private userModel: Model<UserDocument>) {
-    // User defined in user.module.ts
-  }
-  async addUser(name: string, email: string, role: string): Promise<string> {
-    const newUser = new this.userModel({ name, email, role }); // doc will be expanded to name: name etc.
-    const result = await newUser.save();
-    // return mongodb generated id note the underscore.
-    return result._id;
-  }
-
-  async getAllUsers(): Promise<any> {
-    //fix: Use serialization to mask password, so we don't have to transform the data
-    const users: UserDocument[] = await this.userModel.find().exec();
-    return users.map((user) => ({
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      role: user.role,
-    }));
-  }
-  async getUserById(id: string): Promise<UserDocument> {
-    console.log('service id: ', id);
-    let user: UserDocument;
-    try {
-      user = await this.userModel.findById(id).exec();
-    } catch (error) {
-      throw new HttpException('User not found!', 404);
-    }
-    if (!user) {
-      throw new HttpException('User not found!', 404);
-    }
-    return {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      role: user.role,
-    } as UserDocument;
-  }
-
-  async getUserByEmail(email: string): Promise<UserDocument> {
-    let user: UserDocument;
-    try {
-      user = await this.userModel.findOne({ email: email });
-      console.log(user);
-    } catch (error) {
-      throw new HttpException('User not found!', 404);
-    }
-    if (!user) {
-      throw new HttpException('User not found!', 404);
-    }
-
-    return user;
-  }
-  async updateUser(id: string, name: string, email: string, role: Role) {
-    const updatedUser = await this.userModel.findById(id).exec();
-    if (name) {
-      console.log('name ', name);
-      updatedUser.name = name;
-    }
-    if (email) {
-      console.log('email ', email);
-      updatedUser.email = email;
-    }
-    if (role) {
-      console.log('role ', role);
-      updatedUser.role = role;
-    }
-    const updated = await updatedUser.save();
-    console.log(updated);
-    return updated;
-  }
-}
+// import { HttpException, Injectable } from '@nestjs/common';
+// import { Model } from 'mongoose';
+// import { Role, UserDocument } from '../../schemas/user.model';
+// import { InjectModel } from '@nestjs/mongoose';
+//
+// @Injectable()
+// export class UserService {
+//   constructor(@InjectModel('User') private userModel: Model<UserDocument>) {
+//     // User defined in user.module.ts
+//   }
+//   async addUser(name: string, email: string, role: string): Promise<string> {
+//     const newUser = new this.userModel({ name, email, role }); // doc will be expanded to name: name etc.
+//     const result = await newUser.save();
+//     // return mongodb generated id note the underscore.
+//     return result._id;
+//   }
+//
+//   async getAllUsers(): Promise<any> {
+//     //fix: Use serialization to mask password, so we don't have to transform the data
+//     const users: UserDocument[] = await this.userModel.find().exec();
+//     return users.map((user) => ({
+//       id: user.id,
+//       name: user.name,
+//       email: user.email,
+//       role: user.role,
+//     }));
+//   }
+//   async getUserById(id: string): Promise<UserDocument> {
+//     console.log('service id: ', id);
+//     let user: UserDocument;
+//     try {
+//       user = await this.userModel.findById(id).exec();
+//     } catch (error) {
+//       throw new HttpException('User not found!', 404);
+//     }
+//     if (!user) {
+//       throw new HttpException('User not found!', 404);
+//     }
+//     return {
+//       id: user.id,
+//       name: user.name,
+//       email: user.email,
+//       role: user.role,
+//     } as UserDocument;
+//   }
+//
+//   async getUserByEmail(email: string): Promise<UserDocument> {
+//     let user: UserDocument;
+//     try {
+//       user = await this.userModel.findOne({ email: email });
+//       console.log(user);
+//     } catch (error) {
+//       throw new HttpException('User not found!', 404);
+//     }
+//     if (!user) {
+//       throw new HttpException('User not found!', 404);
+//     }
+//
+//     return user;
+//   }
+//   async updateUser(id: string, name: string, email: string, role: Role) {
+//     const updatedUser = await this.userModel.findById(id).exec();
+//     if (name) {
+//       console.log('name ', name);
+//       updatedUser.name = name;
+//     }
+//     if (email) {
+//       console.log('email ', email);
+//       updatedUser.email = email;
+//     }
+//     if (role) {
+//       console.log('role ', role);
+//       updatedUser.role = role;
+//     }
+//     const updated = await updatedUser.save();
+//     console.log(updated);
+//     return updated;
+//   }
+// }
