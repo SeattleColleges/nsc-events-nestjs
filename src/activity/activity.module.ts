d4e28290d370dca579d7e063032269db523a16f7
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-09-05 10:21:04 -0700

    imported AuthModule

--- src/activity/activity.module.ts
+++ src/activity/activity.module.ts
@@ -3,9 +3,11 @@ import { ActivityController } from './controllers/activity/activity.controller';
 import { ActivityService } from './services/activity/activity.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ActivitySchema } from './schemas/activity.schema';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
+    AuthModule,
     MongooseModule.forFeature([{ name: 'Activity', schema: ActivitySchema }]),
   ],
   controllers: [ActivityController],
