361af0a809347f62620b7bdaeaad89b4e7f07302
Jeremy Ward <exochos@gmail.com>
2023-10-10 18:11:15 -0700

    Update .eslintrc.js
    added routes to create a user and delete a user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { SignUpDto } from './dto/signup.dto';
@@ -16,5 +16,19 @@ export class AuthController {
   @Get('/login')
   login(@Body() loginDto: LoginDto): Promise<{ token: string }> {
     return this.authService.login(loginDto);
+  }
+
+  @Post('admindelete')
+  async deleteUser(@Body('email') userEmail: string, @Req() req): Promise<any> {
+    const token = req.headers.authorization.split(' ')[1];
+    await this.authService.adminDeleteUser(userEmail, token);
+    return { success: true, message: 'User successfully deleted.' };
+  }
+
+  @Post('adminadduser')
+  async adminAddUser(@Body() signUpDto: SignUpDto, @Req() req): Promise<any> {
+    const token = req.headers.authorization.split(' ')[1];
+    const user = await this.authService.adminAddUser(signUpDto, token);
+    return { success: true, message: 'User successfully added.', user };
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,10 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './schemas/user.schema';
@@ -53,4 +59,62 @@ export class AuthService {
 
     return { token };
   }
+
+  // Admin only routes
+
+  // Admin add user
+  async adminAddUser(signUpDto: SignUpDto, token: string): Promise<User> {
+    const { name, email, password, role } = signUpDto;
+    // Verify the token
+    let decoded;
+    try {
+      decoded = this.jwtService.verify(token);
+    } catch (error) {
+      throw new UnauthorizedException('Invalid token');
+    }
+
+    // Check the user's role
+    const userId = decoded.id;
+    const requestor = await this.userModel.findById(userId);
+
+    if (!requestor || requestor.role !== 'admin') {
+      throw new ForbiddenException('Only admins can add users');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await this.userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    return user;
+  }
+
+  // Delete a user by email
+  async adminDeleteUser(userEmail: string, token: string): Promise<void> {
+    // Decode the token
+    let decoded;
+    try {
+      decoded = this.jwtService.verify(token);
+    } catch (error) {
+      throw new BadRequestException('Invalid token');
+    }
+
+    const requestorId = decoded.id;
+    const requestor = await this.userModel.findById(requestorId);
+
+    if (!requestor || requestor.role !== 'admin') {
+      throw new UnauthorizedException('Only admins can delete users');
+    }
+
+    const result = await this.userModel.deleteOne({ email: userEmail });
+
+    if (result.deletedCount === 0) {
+      throw new NotFoundException('User not found');
+    }
+  }
+
+  // Update creator role
 }
