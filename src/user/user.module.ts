d64aba2bcbe0655eb80482a68428a524b09459d3
Jeremy Ward <exochos@gmail.com>
2023-10-16 14:52:23 -0700

    refactored user (#31)
    refactored /src/auth/user.schema to /r/auth/UserAuthSchema

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,10 @@
 // import { MongooseModule } from '@nestjs/mongoose';
 // import { UserController } from './controllers/user/user.controller';
 // import { UserService } from './services/user/user.service';
-// import { UserSchema } from './schemas/user.model';
+// import { UserAuthSchema } from './schemas/user.model';
 //
 // @Module({
-//   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+//   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserAuthSchema }])],
 //   controllers: [UserController],
 //   providers: [UserService],
 // })
