d36ced126c1cd0a8f1fe55db102245240886d643
Jeremy Ward <exochos@gmail.com>
2023-10-26 10:48:59 -0700

    Added AuthGuard to user routes
    Since all routes can be guarded from a top level authGuard which trickles down to all of the below user routes. Deleted un-needed authguards.

--- src/user/controllers/user/user.controller.ts
+++ src/user/controllers/user/user.controller.ts
@@ -8,20 +8,21 @@ import {
   Patch,
   Post,
   Req,
+  UseGuards,
 } from '@nestjs/common';
 import { UserService } from '../../services/user/user.service';
 import { Role } from '../../schemas/user.model';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 // ================== User admin routes ======================== \\
-// TODO: Add AuthGuard to all admin routes and check for admin roles
 @Controller('users')
+@UseGuards(AuthGuard('jwt'))
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   // ----------------- Add User ------------------------------- \\
   @Post('new')
-  // @UseGuards(AuthGuard())
   async adminAddUser(@Body() createUserDto: CreateUserDto, @Req() req: any) {
     const generatedId = await this.userService.newUser(createUserDto);
     return { id: generatedId };
@@ -29,28 +30,24 @@ export class UserController {
 
   // ----------------- Get Users ----------------------------- \\
   @Get('')
-  // @UseGuards(AuthGuard())
   async getAllUsers() {
     return await this.userService.getAllUsers();
   }
 
   // ----------------- Get User ------------------------------ \\
   @Get('find/:id')
-  // @UseGuards(AuthGuard())
   async getUserById(@Param('id') id: string) {
     return this.userService.getUserById(id);
   }
 
   // ----------------- Get User by Email --------------------- \\
   @Get('email/:email')
-  // @UseGuards(AuthGuard())
   async getUserByEmail(@Param('email') email: string) {
     return await this.userService.getUserByEmail(email);
   }
 
   // ----------------- Update User --------------------------- \\
   @Patch('update/:id')
-  // @UseGuards(AuthGuard())
   async updateUser(
     @Param('id') id: string,
     @Body('name') name: string,
@@ -62,7 +59,6 @@ export class UserController {
 
   // ----------------- Delete User --------------------------- //
   @Delete('remove/:id')
-  // @UseGuards(AuthGuard())
   async adminDeleteUser(@Param('id') id: string, @Req() req: any) {
     await this.userService.removeUser(id);
   }
