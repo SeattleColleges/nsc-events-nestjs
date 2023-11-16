2212dc0196c64290bfb3f1be4ebeb021c78a3701
Tin Pham <nhattin55@gmail.com>
2023-11-16 14:47:23 -0800

    delete unused GET

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { SignUpDto } from './dto/signup.dto';
