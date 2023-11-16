7b8492ba988ab65e09e388421707fef006ca9a8a
Tin Pham <nhattin55@gmail.com>
2023-11-16 14:42:52 -0800

    change GET to POST for login endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -13,7 +13,7 @@ export class AuthController {
     return this.authService.signUp(signUpDto);
   }
 
-  @Get('/login')
+  @Post('/login')
   login(@Body() loginDto: LoginDto): Promise<{ token: string }> {
     return this.authService.login(loginDto);
   }
