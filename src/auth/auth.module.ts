3d164dae6d822d79b84ce88f0c3286539280f8f6
TroyFPV48 <77989401+TroyFPV48@users.noreply.github.com>
2023-12-05 16:11:24 -0800

    fixed compile error

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
+import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserAuthSchema } from './schemas/userAuth.model';
