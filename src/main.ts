13f87a21b591382ba856a0c5cc81061a549e13c9
Jesse <jesse.caddell@wsu.edu>
2025-01-04 20:21:51 -0800

    reverting changes

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-//another test
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
