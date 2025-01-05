21309ef187d04c45d2145e2eba41f75bd7fda015
Jesse <jesse.caddell@wsu.edu>
2025-01-04 20:19:52 -0800

    testing actions

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+//another test
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
