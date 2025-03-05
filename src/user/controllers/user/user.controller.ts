7990458db9ad649dd339ffc90e831004c46d3c95
Asfand Tanwer <asfandtanwer@yahoo.com>
2025-03-04 23:18:17 -0800

    "Added back rolegaurd decorators and enum"

--- src/user/controllers/user/user.controller.ts
+++ src/user/controllers/user/user.controller.ts
@@ -19,9 +19,11 @@ import { UpdateUserDto } from '../../dto/update-user.dto';
 import { Roles } from '../../../auth/roles.decorator';
 import { RoleGuard } from '../../../auth/role.guard';
 import { Request } from 'express';
+import { UserRole } from '../../../enums/roles.enum';
 
 // ================== User admin routes ======================== \\
-
+@Roles('admin')
+@UseGuards(AuthGuard('jwt'), RoleGuard)
 @Controller('users')
 export class UserController {
   constructor(
@@ -29,6 +31,8 @@ export class UserController {
   ) {}
 
   // ----------------- Get Users ----------------------------- \\
+  @Roles('admin')
+  @UseGuards(AuthGuard('jwt'), RoleGuard)
   @Get('')
   async getAllUsers() {
     return await this.userService.getAllUsers();
@@ -39,6 +43,12 @@ export class UserController {
   async searchUsers(@Req() req: Request) {
     console.log('Search Users Request Received:', req.query);
     const filters: UserSearchFilters = req.query;
+    
+    // Validate the role
+    if (filters.role && !Object.values(UserRole).includes(filters.role as UserRole)) {
+      filters.role = ''; // Defaults to fetching all users
+    }
+    
     return this.userService.searchUsers(filters);
   }
 
