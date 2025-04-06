73c9aa3771d01fc07f0dea413f068e33df582df2
Bennett <33855430+bennettsf@users.noreply.github.com>
2025-04-06 02:44:08 -0700

    fix to user api guard, not supposed to guard the entire user controller (#178)

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors(/*{
+  app.enableCors(
+    /*{
   // TODO: set env variable here
     origin: 'http://localhost:3001', // the frontend (next.js) server
-  }*/);
+  }*/ { credentials: true },
+  );
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
